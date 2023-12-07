36393b6c61f2d922cc7f6e3c2f0ad4171b032627
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-07 15:21:06 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,15 @@
 import streamlit as st
 
-import streamlit as st
-
 st.set_page_config(
     page_title='st.experimental_connection',
     page_icon='🔌'
 )
 
+st.title("🔌 st.experimental_connection")
+
+
+
+tab1, tab2 = st.tabs([
+    "🚀 With st.experimental_connection",
+    "🐢 How it worked before"
+])
