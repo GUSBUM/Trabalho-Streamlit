c0127cd12ef76d403f8688310e5a8be5f7778630
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-07 15:19:59 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-with st.sidebar:
-    with st.echo():
-        st.write("This code will be printed to the sidebar.")
+import streamlit as st
 
-    with st.spinner("Loading..."):
-        time.sleep(5)
-    st.success("Done!")
+st.set_page_config(
+    page_title='st.experimental_connection',
+    page_icon='🔌'
+)
+
