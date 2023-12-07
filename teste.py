f0be4cf4b67360c176dad57eb40f4241f786167b
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-07 15:33:00 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,15 +1,3 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title='st.experimental_connection',
-    page_icon='🔌'
-)
-
-st.title("🔌 st.experimental_connection")
-
-
-
-tab1, tab2 = st.tabs([
-    "🚀 With st.experimental_connection",
-    "🐢 How it worked before"
-])
+st.image('lhama-scaled.jpg')
