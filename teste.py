b1514a65ea3a16c83845c8a5dd3af58d2c029d9d
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-07 14:47:04 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-# Initialize st.session_state.beans
-st.session_state.beans = st.session_state.get("beans", 0)
+a = ddmsadadmdmadmadmadaasdmadmass
 
-st.title("Bean counter :paw_prints:")
-
-addend = st.number_input("Beans to add", 0, 45)
-if st.button("Bimbimbambam"):
-    st.session_state.beans += addend
-st.markdown(f"Beans counted: {st.session_state.beans}")
+st.write(a)
