c15c3677c0e1b53f03d83697480bd853bd899d20
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-08 16:59:40 -0300

    Update 0_Início.py

--- pages/0_Início.py
+++ pages/0_Início.py
@@ -5,3 +5,6 @@ import altair as alt
 
 st.image('./Capturar.PNG')
 st.title('Sobre')
+st.write('
+Bem-vindo à TotalMoney, sua parceira confiável em conversão de moedas! Na TotalMoney, entendemos a importância de maximizar o valor do seu dinheiro em transações internacionais. Somos uma empresa dedicada a oferecer serviços de câmbio eficientes e seguros, proporcionando a você a tranquilidade necessária para lidar com suas finanças globais.
+')
