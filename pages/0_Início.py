77284e3fe800ea80a2b9a6db7c66ec93fd5e37b7
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-12 14:30:40 -0300

    Update 0_Início.py

--- pages/0_Início.py
+++ pages/0_Início.py
@@ -3,11 +3,4 @@ import pandas as pd
 import requests
 from datetime import datetime
 
-requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
-
-requisicao_dic = requisicao.json()
-cotacao_dolar = requisicao_dic["USDBRL"]["bid"]
-cotacao_euro = requisicao_dic["EURBRL"]["bid"]
-cotacao_btc = requisicao_dic["BTCBRL"]["bid"]
-
-st.write({datetime.now()}\nDólar: R$)
+st.image('./Logo.PNG')
