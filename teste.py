7bc38e64ae6f9c9d59dd578b07af2769eba7ff4a
GUSBUM <153019060+GUSBUM@users.noreply.github.com>
2023-12-12 15:24:25 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd 
 
-st.image('./Logo.png')
+st.image('./Capturar.png')
 st.title('Sobre')
 st.text('Nossa empresa especializada em conversão de dinheiro oferece uma abordagem eficiente e segura para atender às necessidades financeiras globais. Conte conosco para uma experiência de conversão de dinheiro sem complicações, onde a confiabilidade e a eficiência são primordiais.')
